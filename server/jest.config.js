4db7e9919d6eaf2f0ba0ba2b4d68be7c5d4d9332
Saroj Tamang <sarojtmg992@gmail.com>
2025-06-24 12:48:25 +0100

    refactor: Minor code adjustments and comment refinements

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,28 +1,19 @@
 module.exports = {
-  preset:'ts-jest',
+  preset: "ts-jest", //use jest
   testEnvironment: "node",
-setupFiles:['<rootDir>/.env', 'dotenv/config'], //points towads the dotenv inside the server folder
- setupFilesAfterEnv: [
-    '<rootDir>/src/test/setup.ts', // Make sure this path is correct
+  setupFiles: ["dotenv/config"], //points towads the dotenv inside the server folder
+  setupFilesAfterEnv: [
+    "<rootDir>/src/test/setup.ts", // points towards setup.ts
   ],
-
-  // Use moduleNameMapper to resolve module paths, especially for absolute imports like `src/...`.
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1', // Maps 'src/...' imports to your actual src folder
+  transform: {
+    "^.+\\.(ts)$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/tsconfig.json", // tells jest to understand ts files
+      },
+    ],
   },
 
-  // Configuration for ts-jest specifically.
-  globals: {
-    'ts-jest': {
-      // Point ts-jest to your backend's tsconfig.json file.
-      // This ensures ts-jest compiles your tests and source code correctly during testing.
-      tsconfig: '<rootDir>/tsconfig.json',
-    },
-  },
-
-
-  testMatch:["**/**/*.test.ts"],
-  verbose:true, //each invidual to be reporte dring run
-
-  
+  testMatch: ["<rootDir>/src/test/**/*.test.ts"], //look in server(rootdir)?src>test>any dir>find file ending with .test.ts and run it
+  verbose: true, //each invidual to be reporte dring run
 };
