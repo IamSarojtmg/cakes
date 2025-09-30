6720243b3b4f8b3d8fde661e631e1793831af12c
Saroj Tamang <sarojtmg992@gmail.com>
2025-09-30 10:21:44 +0100

    feat:Cleaner looking BE homepage

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,28 @@ export{}// got rid of the declared block scope
 const express = require('express')
 const cors = require('cors')
 const cakesRoute = require('./routes/cakeRoutes')
+import { Request, Response } from "express";
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.get("/", async (req:Request, res:Response) => {
+try {
+  res.status(200).json({
+    status: "success",
+    message: "🎂 Cakes API is up and running!",
+    version: "1.0.0",
+  });
+} catch (error) {
+  res.status(500).json({ status: "error", message: "Something went wrong" });
+}
+
+});
+
 app.use("/cakes", cakesRoute);
+
 
 module.exports = app;
